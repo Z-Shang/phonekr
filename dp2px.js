8b67446b3501a06bd04a332ce56249fdb8c3105c
Z.Shang <shangzhanlin@gmail.com>
2013-05-24 16:58:42 -0500

    first

---
+++ dp2px.js
@@ -0,0 +1,44 @@
+function calcpx(){
+var dp=document.getElementById("dp");
+var pxl=document.getElementById("pxl");
+var pxm=document.getElementById("pxm");
+var pxh=document.getElementById("pxh");
+var pxxh=document.getElementById("pxxh");
+var pxxxh=document.getElementById("pxxxh");
+pxl.value = dp.value * 3 / 4;
+pxm.value = dp.value * 1;
+pxh.value = dp.value * 3 / 2;
+pxxh.value = dp.value * 2;
+pxxxh.value = dp.value * 3;
+}
+function cl(){
+var dp=document.getElementById("dp");
+var pxl=document.getElementById("pxl");
+var pxm=document.getElementById("pxm");
+var pxh=document.getElementById("pxh");
+var pxxh=document.getElementById("pxxh");
+var pxxxh=document.getElementById("pxxxh");
+dp.value="";
+pxl.value="";
+pxm.value="";
+pxh.value="";
+pxxh.value="";
+pxxxh.value="";
+}
+function calcdp(idtf, dpi){
+var dp=document.getElementById("dp");
+var pxl=document.getElementById("pxl");
+var pxm=document.getElementById("pxm");
+var pxh=document.getElementById("pxh");
+var pxxh=document.getElementById("pxxh");
+var pxxxh=document.getElementById("pxxxh");
+dp.value=idtf.value * 160 / dpi;
+}
+function cz(){
+var dp=document.getElementById("dp");
+if((dp.value - Math.floor(dp.value)) > 0.5){
+dp.value=Math.ceil(dp.value);
+}else if((dp.value - Math.floor(dp.value)) < 0.5){
+dp.value=Math.floor(dp.value);
+}
+}
